7551313dd345a4b674843b2c229b3398c9c198f0
unnamed-ii <isaingashev@gmail.com>
2023-07-07 01:31:33 +0300

    added changing bg-color for header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import './header.scss';
 import Container from "../Container/container";
 import {ReactComponent as Logo} from '../../images/icons/logo.svg';
 import {HeaderData} from "../../constants";
 
 const Header = () => {
+    const [onScroll, setOnScroll] = useState(false);
+    const changeBackgroundColor = () => {
+        if (window.scrollY > 100) setOnScroll(true)
+        if (window.scrollY < 100) setOnScroll(false)
+    }
+    window.addEventListener('scroll', changeBackgroundColor)
 
     return (
         <Container>
-            <header className="header">
+            <header className={"header" + (onScroll ? " on-scroll" : "")}>
                 <a href="#main"><Logo/></a>
                 <nav className="header__nav">
                     <ul className="header__nav-list">
