1010bc6171875c3e26dd705b9ba3e9495c94d5e1
unnamed-ii <isaingashev@gmail.com>
2023-07-06 19:35:15 +0300

    created constants file for info-video block and refactored layout

--- src/blocks/InfoVideo/infovideo.jsx
+++ src/blocks/InfoVideo/infovideo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './infovideo.scss';
 import Container from "../../components/Container/container";
 import videoImage from '../../images/video-img.png';
+import {InfoVideoData} from "../../constants";
 
 const InfoVideo = () => {
     return (
@@ -10,22 +11,12 @@ const InfoVideo = () => {
                 <div className="info-video__inner">
                     <img src={videoImage} alt="" className="info-video__inner-image"/>
                     <ul className="info-video__inner-information">
-                        <li>
-                            450
-                            <span>туристов</span>
-                        </li>
-                        <li>
-                            2500
-                            <span>часов катания</span>
-                        </li>
-                        <li>
-                            10
-                            <span>проф. тренеров</span>
-                        </li>
-                        <li>
-                            365
-                            <span>дней в году</span>
-                        </li>
+                        {InfoVideoData.map(element => (
+                            <li>
+                                {element.number}
+                                <span>{element.text}</span>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </Container>
